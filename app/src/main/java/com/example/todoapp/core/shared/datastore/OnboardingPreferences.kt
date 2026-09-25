a879a947bea37d5b27194f04c0216afcfc1b6e4a
Hassam <hassamnaeem047@gmail.com>
2026-09-25 12:27:57 +0500

    Injected DataStore into OnboardingDataPreferences

--- app/src/main/java/com/example/todoapp/core/shared/datastore/OnboardingPreferences.kt
+++ app/src/main/java/com/example/todoapp/core/shared/datastore/OnboardingPreferences.kt
@@ -1,28 +1,27 @@
 package com.example.todoapp.core.shared.datastore
 
-import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.preferencesDataStore
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
-private val Context.dataStore by preferencesDataStore("todo_app_preferences")
 
 @Singleton
 class OnboardingDataPreferences @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val dataStore: DataStore<Preferences>
 ) {
     private val onboardingCompletedKey = booleanPreferencesKey("onboarding_completed")
-    val isOnboardingCompleted: Flow<Boolean> = context.dataStore.data.map { preferences ->
-        preferences[onboardingCompletedKey] ?: false
-    }
+    val isOnboardingCompleted: Flow<Boolean> =
+        dataStore.data.map { preferences ->
+            preferences[onboardingCompletedKey] ?: false
+        }
 
     suspend fun setOnboardingCompleted() {
-        context.dataStore.edit { preferences ->
+        dataStore.edit { preferences ->
             preferences[onboardingCompletedKey] = true
         }
     }
